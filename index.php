61c9242484423c3c403a7553b9c14930bc1a6e9e
alessandronob <alessandrobastos22@gmail.com>
2018-06-21 21:07:20 -0300

    Inicio Projeto PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+	$titulo = "Titulo da Página";
+	$tituloConteudo = "<h1>titulo do Texto</h1>";
+	$conteudo = "<p>GIT - Versionamento</p>";
+
+?>
+<html>
+<head>
+	<title><?php echo $ titulo; ?></title>
+	<meta charset="utf-8" />
+</head>
+</body>
+<?php
+	echo $tituloConteudo;
+	echo $conteudo;
+?>
+</body>
+</html>
+	
